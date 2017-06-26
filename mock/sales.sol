578cfe6ffa1084eb93d39d969aead95ed77fd1f9
ajs <ajsantos@posteo.net>
2017-06-26 01:19:50 -0500

    Update sales.sol

--- mock/sales.sol
+++ mock/sales.sol
@@ -1,45 +1,17 @@
-/*
-SECURED INTERNATIONAL SALE OF GOODS SMART CONTRACT
-
-This agreement for the sale of goods is entered between the Seller (“<enter address public seller>”) and Buyer (“<enter address public buyer>”).
-
-The parties agree as follows:
-
-1. Sale of Goods. The Seller shall sell to the Buyer and the Buyer shall purchase from the Seller the goods in the quantities and at the prices as set forth in IPFS: <enter hash>.
-
-2. Deposit. Each party shall transmit funds to <enter contract address> in the amount of the price of the goods and a security deposit that is equal to the value of the goods. The Seller may cancel this agreement before a deposit is made by the Buyer and receive paid in funds back. Once a deposit by the Buyer is made, the funds shall be locked in the contract’s wallet.
-
-3. Delivery; Title; and Risk of Loss. The Seller shall deliver the goods under Incoterms 2010 <enter delivery term>.
-
-4. Payment. Upon delivery of the goods, the Buyer shall execute the function to release the locked funds from the contract’s wallet. The Seller shall receive the price of the goods and Seller’s portion of the security deposit. The Buyer shall receive the Buyer’s portion of the security deposit.
-
-5. Limitation of Liability. The Seller shall not be liable for any indirect, special, consequential, or punitive damages (including lost profits) arising out of or relating to this agreement, whether for breach of contract, tort, negligence, or any other form of action. In no event shall the Seller’s liability exceed the price the Buyer paid for the specific goods provided by the Seller giving rise to the claim or cause of action.
-
-6. Force Majeure. The Seller shall not be held liable for delays in performance or for non-performance due to unforeseen circumstances or causes beyond the Seller’s reasonable control.
-
-7. Plain Text. This plain text version of the agreement supersedes any other rules, electronic logic, code, programs, or software representations of the terms and conditions found herein.
-
-8. Governing Law. The governing law for the agreement shall be the UNIDROIT Principles (2010) and, with respect to issues not covered by such Principles, by generally accepted principles of international commercial law.
-
-9. Dispute Resolution. Any dispute arising out of or in connection with this agreement shall be referred to and finally resolved by arbitration under the latest version of the Arbitracio Rules found at https://github.com/alvinjoelsantos/arbitracio, which Rules are deemed to be incorporated by reference. The parties agree that the arbitration shall be conducted with the use of electronic documents only and that the arbitral tribunal may limit the number, length, and scope of written submissions and written witness evidence. The arbitral tribunal may directly enforce any award by executing functions from the arbitrator’s wallet (“enter address public arbitrator”) to release funds to the respective parties in accordance with such award. To initiate the arbitration process either party may submit 2 ether to the contract’s wallet, which will lock the funds until the dispute is resolved. The arbitration fee is set to 2 ether and shall be equally split between the Seller and Buyer. On distribution of funds, the arbitrator may deduct the arbitration fee from the security deposit.
-
-10. Electronic Signatures. This agreement is signed electronically by the parties and recorded on <enter blockchain and transaction hash>.
-*/
-
 pragma solidity ^0.4.11;
 
-contract Sales {
+contract SaleOfGoods {
     uint public value;
-    bytes32 public pid = 4b57c7141cf17c97f76ab9c5f6bdbfb3a5e92078fb8657a594581a9977c50b92;
     address public seller;
     address public buyer;
     address public arbitrator;
-    enum State { Created, Locked, Inactive }
+    enum State { Created, Locked, Inactive, Disputed }
     State public state;
 
-    function Purchase() payable {
+    function SaleOfGoods() payable {
         seller = msg.sender;
         value = msg.value / 2;
+        arbitrator = 0x237f4d5F1BD369A167260A0D7fcECf83BB5f14eD;   //Hard-code the arbitrator's address
         require((2 * value) == msg.value);
     }
 
@@ -68,38 +40,36 @@ contract Sales {
         _;
     }
 
-    event Revoked();
-    event Accepted();
+    event SellerRevokedOffer();
+    event BuyerAcceptedOffer();
     event GoodsReceived();
-    event Disputed();
-    event Resolved();
-    event Terminated();
+    event DisputeResolutionProcessInitiated();
+    event ArbitralAwardEnforced(uint sellerBalance, uint buyerBalance);
+    event ContractRescinded(uint balanceToSend);
 
     /// Seller can revoke the offer and recover deposit before Buyer's acceptance.
-    function revoke()
+    function sellerRevokeOffer()
         onlySeller
         inState(State.Created)
     {
-        Revoked();
+        SellerRevokedOffer();
         state = State.Inactive;
         seller.transfer(this.balance);
     }
 
     /// Buyer submits deposit to accept Seller's offer, which lock funds until Seller's performance. Buyer's deposit is twice the value of the goods.
-    function acceptOffer()
-    function accept()        
+    function buyerAcceptOffer()
         inState(State.Created)
         condition(msg.value == (2 * value))
         payable
     {
-        Accepted();
+        BuyerAcceptedOffer();
         buyer = msg.sender;
         state = State.Locked;
     }
 
     /// Buyer's confirmation of delivery and release of funds. Seller receives price of goods and deposit. Buyer receives deposit.
-    function confirmDelivery()
-    function delivered()
+    function buyerConfirmDelivery()
         onlyBuyer
         inState(State.Locked)
     {
@@ -110,35 +80,47 @@ contract Sales {
     }
 
     /// Dispute resolution.
-    function dispute()
+    function initiateDispute()
         inState(State.Locked)
-        condition(msg.value == (2))
-        payable
+        {
+        DisputeResolutionProcessInitiated();
+        state = State.Disputed;    //Set the state to Disputed
+    }
+
+    function arbitratorResolveDispute(uint deductFromSeller ,uint addToBuyer, uint deductFromBuyer)              // Pass each value for their respective operation, otherwise just pass 0
+        onlyArbitrator
+        inState(State.Disputed)                 // should be called in case when state is Disputed
     {
-        Disputed();
-        state = State.Locked;
-    }
-    
-    function resolve()
-        onlyArbitrator
-        inState(State.Locked)
-    {        
-        Resolved();
+        uint sellerBalance = (((value*2)+value)/(1 ether) - deductFromSeller) * 1000000000000000000;   // 1000000000000000000 is for converting Wei to ether
+
+       uint buyerBalance = (value/(1 ether) + addToBuyer - deductFromBuyer) * 1000000000000000000;// 1000000000000000000 is for converting Wei to ether
+
+        ArbitralAwardEnforced(sellerBalance,buyerBalance);          // Fire resolved event
+
         state = State.Inactive;
-        buyer.transfer(value - 1);
-        seller.transfer((value * 2) - 1);
+        seller.transfer(sellerBalance);     //send balance to seller
+        buyer.transfer(buyerBalance);   // send balance to buyer
         arbitrator.transfer(this.balance);
     }
-    
-    function terminate()
+
+
+    function arbitratorRescindContract(uint deductAmount)
         onlyArbitrator
-        inState(State.Locked)
-    {          
-        Terminated();
+        inState(State.Disputed)
+    {
+
+        // It will deduct the same amount from both Buyer and Seller
+         uint balanceToSend = (value*2) - deductAmount * 1000000000000000000;// 1000000000000000000 is for converting Wei to ether
+
+        ContractRescinded(balanceToSend);
         state = State.Inactive;
-        buyer.transfer((value * 2) - 1);
-        seller.transfer((value * 2) - 1);
+        buyer.transfer(balanceToSend);
+        seller.transfer(balanceToSend);
         arbitrator.transfer(this.balance);
     }
-        
+
+    function arbitratorDeleteContract(){
+         selfdestruct(arbitrator);
+    }
+
 }
